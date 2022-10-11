812285f83dd1e6f09bf3940850df3a1b815f9c81
Spago123 <hspago1@etf.unsa.ba>
2022-10-11 21:30:04 +0200

    Initial commit

--- src/rpr/unsa/etf/ba/Main.java
+++ src/rpr/unsa/etf/ba/Main.java
@@ -2,6 +2,6 @@ package rpr.unsa.etf.ba;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        System.out.println("Vozdra raja");
     }
 }
